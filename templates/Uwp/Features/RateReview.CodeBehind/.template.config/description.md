1d8d1a9d29786c31a09f6e62629243a33f095b42
Matt Lacey <matt@mrlacey.co.uk>
2018-06-05 14:23:28 +0100

    codebehind functionality

---
+++ templates/Uwp/Features/RateReview.CodeBehind/.template.config/description.md
@@ -0,0 +1,5 @@
+﻿Add a link to the settings page that will open a dialog to capture reviews and ratings for your app in the store.
+
+This is an alternative to [launching the store app](https://docs.microsoft.com/en-us/windows/uwp/launch-resume/launch-store-app) for the review or rating to be entered.
+
+Note. This functionality can only be used in apps distributed through the store.
---
+++ templates/Uwp/Features/RateReview.CodeBehind/Views/Param_SettingsPageNamePage_postaction.xaml.cs
@@ -0,0 +1,30 @@
+﻿//{**
+// This code block adds the code to launch the rate and review dialog from the settings page
+//**}
+namespace Param_ItemNamespace.Views
+{
+    public sealed partial class Param_SettingsPageNamePage : Page, INotifyPropertyChanged
+    {
+        //^^
+        //{[{
+
+        private async void RateReviewLink_Click(object sender, RoutedEventArgs e)
+        {
+            // For more details see https://docs.microsoft.com/en-us/windows/uwp/monetize/request-ratings-and-reviews
+            var result = await StoreRequestHelper.SendRequestAsync(StoreContext.GetDefault(), 16, String.Empty);
+
+            // TODO WTS: Handle the response from showing the dialog (optional)
+            if (result.ExtendedError == null)
+            {
+                JObject jsonObject = JObject.Parse(result.Response);
+
+                if (jsonObject.SelectToken("status").ToString() == "success")
+                {
+                    // The customer rated or reviewed the app.
+                    // See the above link for more details about possible server responses.
+                }
+            }
+        }
+        //}]}
+    }
+}
