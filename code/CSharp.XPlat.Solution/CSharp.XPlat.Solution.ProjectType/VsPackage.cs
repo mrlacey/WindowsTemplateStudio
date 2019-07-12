561638ce3be16227cd47928bb610e37a2956869d
Matt Lacey <matt@mrlacey.co.uk>
2019-07-12 21:25:24 +0100

    WIP

---
+++ code/CSharp.XPlat.Solution/CSharp.XPlat.Solution.ProjectType/VsPackage.cs
@@ -0,0 +1,54 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+This code is licensed under the Visual Studio SDK license terms.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+namespace CSharpXPlatSolution
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// This package is required if you want to define adds custom commands (ctmenu)
+    /// or localized resources for the strings that appear in the New Project and Open Project dialogs.
+    /// Creating project extensions or project types does not actually require a VSPackage.
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Description("A custom project type based on CPS")]
+    [Guid(VsPackage.PackageGuid)]
+    public sealed class VsPackage : Package
+    {
+        /// <summary>
+        /// The GUID for this package.
+        /// </summary>
+        public const string PackageGuid = "82530820-6b78-46b3-9c93-5514c6139938";
+
+        /// <summary>
+        /// The GUID for this project type.  It is unique with the project file extension and
+        /// appears under the VS registry hive's Projects key.
+        /// </summary>
+        public const string ProjectTypeGuid = "646fe1e1-7dad-4b3e-9979-70a39ac3b7b9";
+
+        /// <summary>
+        /// The file extension of this project type.  No preceding period.
+        /// </summary>
+        public const string ProjectExtension = "csproj";
+
+        /// <summary>
+        /// The default namespace this project compiles with, so that manifest
+        /// resource names can be calculated for embedded resources.
+        /// </summary>
+        internal const string DefaultNamespace = "CSharpXPlatSolution";
+    }
+}
