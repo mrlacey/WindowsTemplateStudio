b555cb564f2478b0d1c4aa8ac57f60bf684f4324
Matt Lacey <matt@mrlacey.co.uk>
2019-09-17 15:49:05 +0100

    Basic Uno Map Page

---
+++ templates/Xplat/Pages/Uno.Map/Param_ProjectName.Shared/ViewModels/MapPageViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls.Maps;
+
+namespace Param_RootNamespace.ViewModels
+{
+    public class MapPageViewModel : BaseViewModel
+    {
+        // TODO WTS: Set your preferred default zoom level
+        private const double DefaultZoomLevel = 17;
+
+        // TODO WTS: Set your preferred default location if a geolock can't be found.
+        private readonly BasicGeoposition _defaultPosition = new BasicGeoposition()
+        {
+            Latitude = 45.5068211,
+            Longitude = -73.5694924
+        };
+
+        private double _zoomLevel;
+
+        public double ZoomLevel
+        {
+            get { return _zoomLevel; }
+            set { SetProperty(ref _zoomLevel, value); }
+        }
+
+        private Geopoint _center;
+
+        public Geopoint Center
+        {
+            get { return _center; }
+            set { SetProperty(ref _center, value); }
+        }
+
+        public MapPageViewModel()
+        {
+            Center = new Geopoint(_defaultPosition);
+            ZoomLevel = DefaultZoomLevel;
+        }
+
+        public async Task InitializeAsync(MapControl map)
+        {
+            // TODO: get (and track changes in) location
+
+            if (map != null)
+            {
+                // TODO WTS: Set your map service token. If you don't have one, request from https://www.bingmapsportal.com/
+                // map.MapServiceToken = string.Empty;
+                AddMapIcon(map, Center, "You are here");
+            }
+        }
+
+        public void Cleanup()
+        {
+            // TODO: If appropriate, unsubscribe from tracking location
+        }
+
+        private void AddMapIcon(MapControl map, Geopoint position, string title)
+        {
+            var mapIcon = new MapIcon()
+            {
+                Location = position,
+                NormalizedAnchorPoint = new Point(0.5, 1.0),
+                Title = title,
+                Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/map.png")),
+                ZIndex = 0
+            };
+            map.MapElements.Add(mapIcon);
+        }
+    }
+}
---
+++ templates/Xplat/Pages/Uno.Map/Param_ProjectName.Shared/Views/MapPagePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Param_RootNamespace.ViewModels;
+
+namespace Param_RootNamespace.Views
+{
+    public sealed partial class MapPagePage : Page
+    {
+        private MapPageViewModel ViewModel = new MapPageViewModel();
+        
+        public MapPagePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await ViewModel.InitializeAsync(mapControl);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            ViewModel.Cleanup();
+        }
+    }
+}
