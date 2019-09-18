9034cf4657520ea5632b22e2e4aaf5046c9e2eac
Matt Lacey <matt@mrlacey.co.uk>
2019-09-18 04:38:33 +0100

    Uno Settings page

---
+++ templates/Xplat/Pages/Uno.Settings/Param_ProjectName.Shared/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Param_RootNamespace.Helpers;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+
+namespace Param_RootNamespace.ViewModels
+{
+    // TODO WTS: Add settings as necessary. For help see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/pages/settings.md
+    public class SettingsPageViewModel : BaseViewModel
+    {
+        private string _versionDescription;
+
+        public string VersionDescription
+        {
+            get { return _versionDescription; }
+
+            set { SetProperty(ref _versionDescription, value); }
+        }
+
+        public SettingsPageViewModel()
+        {
+        }
+
+        public async Task InitializeAsync()
+        {
+            VersionDescription = GetVersionDescription();
+            await Task.CompletedTask;
+        }
+
+        private string GetVersionDescription()
+        {
+            var appName = "AppDisplayName".GetLocalized();
+            var package = Package.Current;
+            var packageId = package.Id;
+            var version = packageId.Version;
+
+            return $"{appName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+    }
+}
---
+++ templates/Xplat/_comp/Uno/NavView.Page-Settings/Param_ProjectName.Shared/Views/ShellPage_postaction.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Param_RootNamespace.Views
+{
+    public sealed partial class ShellPage : Page
+    {
+        public void OnSelectionChanged(object sender, NavigationViewSelectionChangedEventArgs args)
+        {
+//{[{
+            if (args.IsSettingsSelected)
+            {
+                this.ContentFrame.Navigate(typeof(SettingsPage), null, null);
+                return;
+            }
+
+//}]}
+        }
+    }
+}
