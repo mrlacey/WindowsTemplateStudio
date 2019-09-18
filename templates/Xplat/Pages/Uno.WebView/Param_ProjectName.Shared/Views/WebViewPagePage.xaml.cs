2dd68d8b87dd1f1ea71e9b4fcc46605dd70153d7
Matt Lacey <matt@mrlacey.co.uk>
2019-09-18 06:19:19 +0100

    Uno WebView page

---
+++ templates/Xplat/Pages/Uno.WebView/Param_ProjectName.Shared/Views/WebViewPagePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Param_RootNamespace.ViewModels;
+
+namespace Param_RootNamespace.Views
+{
+    public sealed partial class WebViewPagePage : Page
+    {
+        private WebViewPageViewModel ViewModel = new WebViewPageViewModel();
+
+        public WebViewPagePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
